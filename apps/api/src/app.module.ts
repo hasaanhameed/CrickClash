d8ea2d7f714f0e6032c68af4a4180a0df8bf478f
Hasaan Hameed <hasaanhameed52@gmail.com>
2026-07-27 10:45:48 +0500

    Update: Daily challenge configured

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { QuizPacksModule } from './quiz-packs/quiz-packs.module';
+import { DailyChallengeModule } from './daily-challenge/daily-challenge.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, QuizPacksModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, QuizPacksModule, DailyChallengeModule],
   controllers: [AppController],
   providers: [AppService],
 })
