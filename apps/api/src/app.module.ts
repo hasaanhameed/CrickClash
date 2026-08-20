6d045ada79244cb5f09f1d07002fc4a4363c886e
Hasaan Hameed <hasaanhameed52@gmail.com>
2026-08-20 11:29:39 +0500

    Update: QuizPackMatch Module added

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { QuizPacksModule } from './quiz-packs/quiz-packs.module';
 import { DailyChallengeModule } from './daily-challenge/daily-challenge.module';
+import { QuizPackMatchModule } from './quiz-pack-match/quiz-pack-match.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, QuizPacksModule, DailyChallengeModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, QuizPacksModule, DailyChallengeModule, QuizPackMatchModule],
   controllers: [AppController],
   providers: [AppService],
 })
